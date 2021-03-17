af0450a1b066b70dd7b26b18975605c0c0fe5f07
mali <mohsin_ali_nisar@yahoo.com>
2021-03-17 18:07:49 +0500

    Add course model

--- CleanArch/CleanArch.Domain/Model/Course.cs
+++ CleanArch/CleanArch.Domain/Model/Course.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace CleanArch.Domain.Model
 {
-  public  class Course
+    public class Course
     {
         public int Id { get; set; }
         public string Name { get; set; }
